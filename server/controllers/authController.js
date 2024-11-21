0d9cdeb3ac1ccc0db856d8b7d49f643275b57280
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-22 04:34:40 +0530

    add login functionality with password comparison

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,5 +1,5 @@
 import { User } from "../models/index.js";
-import { hashPassword } from "../utils/hashPassword.js";
+import { comparePassword, hashPassword } from "../utils/hashPassword.js";
 import generateToken from "../utils/generateToken.js";
 
 export const register = async (req, res) => {
@@ -43,4 +43,36 @@ export const register = async (req, res) => {
     res.status(500).json({ message: "Internal server error" });
   }
 };
-export const login = async (req, res) => {};
+export const login = async (req, res) => {
+  try {
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ where: { username } });
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isPasswordCorrect = await comparePassword(password, user.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const userResponse = {
+      ...user.toJSON(),
+    };
+    delete userResponse.password;
+
+    const data = {
+      id: userResponse.id,
+    };
+
+    generateToken(data, res);
+
+    return res
+      .status(200)
+      .json({ message: "Login successful", user: userResponse });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ message: "Internal server error" });
+  }
+};
