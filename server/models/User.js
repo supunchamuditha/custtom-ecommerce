4424f520d86a8e4dcef6be9934993adb636ebbb8
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-22 03:48:25 +0530

    add User model and database synchronization logic

---
+++ server/models/User.js
@@ -0,0 +1,39 @@
+import { DataTypes } from "sequelize";
+import sequelize from "../config/db.js";
+
+const User = sequelize.define(
+  "User",
+  {
+    full_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    phone_number: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    address: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
+
+export default User;
