f9da7a1cc10a6f42425deb72f16f6f5b2ed697e2
Pasith Senevirathna <pasith.senevi02@gmail.com>
2024-11-22 06:24:22 +0530

    add environment configuration, CORS, and cookie parsing middleware

--- server/app.js
+++ server/app.js
@@ -1,8 +1,16 @@
 import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+
 import authRoutes from './routes/authRoute.js'
 
+dotenv.config();
+
 const app = express();
+app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
 app.use('/api/auth', authRoutes);
 
